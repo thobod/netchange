2c31abc7e40e63cde79ba367ce1c664f9c95f7ad
Thomas-PC <tboddeus@gmail.com>
2017-12-11 16:33:37 +0100

    initial push

---
+++ MultiClientServer/MultiClientServer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiClientServer
+{
+    class Program
+    {
+        static public int MijnPoort;
+
+        static public Dictionary<int, Connection> Buren = new Dictionary<int, Connection>();
+
+        static void Main(string[] args)
+        {
+            Console.Write("Op welke poort ben ik server? ");
+
+            MijnPoort = int.Parse(Console.ReadLine());
+            new Server(MijnPoort);
+
+            Console.WriteLine("Typ [verbind poortnummer] om verbinding te maken, bijvoorbeeld: verbind 1100");
+            Console.WriteLine("Typ [poortnummer bericht] om een bericht te sturen, bijvoorbeeld: 1100 hoi hoi");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.StartsWith("verbind"))
+                {
+                    int poort = int.Parse(input.Split()[1]);
+                    if (Buren.ContainsKey(poort))
+                        Console.WriteLine("Hier is al verbinding naar!");
+                    else
+                    {
+                        // Leg verbinding aan (als client)
+                        Buren.Add(poort, new Connection(poort));
+                    }
+                }
+                else
+                {
+                    // Stuur berichtje
+                    string[] delen = input.Split(new char[] { ' ' }, 2);
+                    int poort = int.Parse(delen[0]);
+                    if (!Buren.ContainsKey(poort))
+                        Console.WriteLine("Hier is al verbinding naar!");
+                    else
+                        Buren[poort].Write.WriteLine(MijnPoort + ": " + delen[1]);
+                }
+            }
+        }
+    }
+}
