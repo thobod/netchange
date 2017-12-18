67c2e30ac5abcddf10d5e93194376375216b71c6
Jaimy Kwa <j.t.s.kwa@students.uu.nl>
2017-12-18 16:32:54 +0100

    More.

--- netchangenew/netchangenew/Program.cs
+++ netchangenew/netchangenew/Program.cs
@@ -21,7 +21,53 @@ namespace netchangenew
             {
                 int port = int.Parse(args[i]);
                 Neighbours.Add(port, new Connection(port));
-                LockObjects.Add(port, new object());
+                if(!LockObjects.ContainsKey(port)) LockObjects.Add(port, new object());
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.StartsWith("C"))
+                {
+                    int port = int.Parse(input.Split()[1]);
+                    if (Neighbours.ContainsKey(port))
+                        Console.WriteLine("Hier is al verbinding naar!");
+                    else
+                    {
+                        // Leg verbinding aan (als client)
+                        Neighbours.Add(port, new Connection(port));
+                        if (!LockObjects.ContainsKey(port)) LockObjects.Add(port, new object());
+                    }
+                }
+                else if(input.StartsWith("B"))
+                {
+                    // Stuur berichtje
+                    string[] delen = input.Split(new char[] { ' ' }, 2);
+                    int port = int.Parse(delen[0]);
+                    if (!Neighbours.ContainsKey(port))
+                        Console.WriteLine("Poort " + port + " is niet bekend");
+                    else
+                        Neighbours[port].Write.WriteLine(myPort + ": " + delen[1]);
+                }
+                else if (input.StartsWith("D"))
+                {
+                    //Verwijder port
+                    int port = int.Parse(input.Split()[1]);
+                    if (!Neighbours.ContainsKey(port))
+                        Console.WriteLine("Poort " + port + " is niet bekend");
+                    else
+                    {
+                        Neighbours.Remove(port);
+                    }
+                }
+                else if (input.StartsWith("R"))
+                {
+
+                }
+                else if (input.StartsWith("Update"))
+                {
+
+                }
             }
         }
     }
